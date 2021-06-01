ed6b392b58132ecdd776d64f0c4839426016693f
iletavcioski <it8816@student.uni-lj.si>
2021-06-01 12:25:53 +0200

    se nekaj

--- exercise3/src/map_goals.cpp
+++ exercise3/src/map_goals.cpp
@@ -704,6 +704,7 @@ source /home/iletavcioski/ROS/devel/setup.bash
 roslaunch exercise7 rins_world.launch
 roslaunch exercise3 amcl_simulation.launch 2>/dev/null
 roslaunch turtlebot_rviz_launchers view_navigation.launch 2>/dev/null
+rostopic echo /camera/rgb/image_raw --noarr
 rosrun exercise4 colorLocalizer.py
 rosrun exercise3 map_goals
 rosrun exercise3 move_arm.py
