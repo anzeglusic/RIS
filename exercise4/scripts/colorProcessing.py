93a75d13c5f5248cf8d358ab091ae05e116635f0
anzeglusic <anze.glusic@gmail.com>
2021-06-03 07:49:22 +0200

    analysis done

--- exercise4/scripts/colorProcessing.py
+++ exercise4/scripts/colorProcessing.py
@@ -82,13 +82,23 @@ library = {
 #! OD TUKAJ NAPREJ POČNEŠ KAR HOČEŠ !!!
 # dictionary je v spremenljivki "library"
 allColors = ["r","g","b","y","c"]
+numOfAllDetections = 0
+numOfAllPixels = 0
+for color in allColors:
+    temp1 = library["ring"][color]
+    temp2 = library["cylinder"][color]
+    numOfAllDetections += len(temp1)
+    numOfAllDetections += len(temp2)
+    numOfAllPixels += sum([len(x) for x in temp1])
+    numOfAllPixels += sum([len(x) for x in temp2])
+
 print()
 print(f"--------------------- num of detections ---------------------")
 for objectType in ["ring", "cylinder"]:
     print(f"{objectType}:")
     for color in allColors:
         temp = library[objectType][color]
-        print(f"\t{color}: {len(temp)}")
+        print(f"\t{color}: {len(temp)}\t( {round(len(temp)/numOfAllDetections*100)} % )")
 print()
 
 print(f"--------------------- num of pixels ---------------------")
@@ -96,7 +106,7 @@ for objectType in ["ring", "cylinder"]:
     print(f"{objectType}:")
     for color in allColors:
         temp = [len(x) for x in library[objectType][color]]
-        print(f"\t{color}: {sum(temp)}")
+        print(f"\t{color}: {sum(temp)}\t( {round(sum(temp)/numOfAllPixels*100)} % )")
 print()
 
 print(f"--------------------- (min, max) pixlov in 1 detection ---------------------")
@@ -106,3 +116,18 @@ for objectType in ["ring", "cylinder"]:
         temp = [len(x) for x in library[objectType][color]]
         print(f"\t{color}: ({min(temp) if len(temp)>0 else 0}\t {max(temp) if len(temp)>0 else 0})")
 print()
+
+print(f"--------------------- detections per color ---------------------")
+for color in allColors:
+    print(f"{color}: {len(library['ring'][color])+len(library['cylinder'][color])}\t( {round((len(library['ring'][color])+len(library['cylinder'][color]))/numOfAllDetections*100)} % )")
+print()
+
+print(f"--------------------- pixels per color ---------------------")
+for color in allColors:
+    temp1 = [len(x) for x in library["ring"][color]]
+    temp2 = [len(x) for x in library["cylinder"][color]]
+    print(f"{color}: {sum(temp1)+sum(temp2)}\t( {round((sum(temp1)+sum(temp2))/numOfAllPixels*100)} % )")
+print()
+
+
+
