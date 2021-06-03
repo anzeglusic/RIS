dc39b2ec1e83957eaf5c7a2fec473f7710e782a8
sk9796 <sk9796@student.uni-lj.si>
2021-06-03 11:23:08 +0200

    sum color detection magic

--- exercise4/scripts/colorProcessing.py
+++ exercise4/scripts/colorProcessing.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 
+from pprint import pprint
+import pandas as pd
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from skimage.color import rgb2hsv
+import cv2
+import pickle
 import json
 from pprint import pprint
 import os
+import random
+import functools
+import operator
 modelsDir = '/'.join(os.path.realpath(__file__).split('/')[0:-1])+'/'
 
 alreadyExists = True
 try:
     with open(f"{modelsDir}colorsDataset.json","r",encoding="utf-8") as f:
         library = json.loads(f.read())
-    
+
 except json.decoder.JSONDecodeError as err:
     alreadyExists = False
     print("JSON error: {err}")
@@ -39,7 +52,7 @@ except Exception as err:
 #         }
 #         with open(f"{modelsDir}colorsDataset.json","w",encoding="utf-8") as f:
 #             f.write(json.dumps(library))
-        
+
 #     except Exception as err:
 #         print(err)
 
@@ -56,15 +69,15 @@ library = {
         "g" : [ detection0, detection1, detection2, ...],
         "b" : [ detection0, detection1, detection2, ...],
         "y" : [ detection0, detection1, detection2, ...],
-        "c" : [ detection0, detection1, detection2, ...] 
+        "c" : [ detection0, detection1, detection2, ...]
     },
-    
+
     "cylinder" : {
         "r" : [ detection0, detection1, detection2, ...],
         "g" : [ detection0, detection1, detection2, ...],
         "b" : [ detection0, detection1, detection2, ...],
         "y" : [ detection0, detection1, detection2, ...],
-        "c" : [ detection0, detection1, detection2, ...] 
+        "c" : [ detection0, detection1, detection2, ...]
     }
 }
 
@@ -78,8 +91,67 @@ library = {
 
         detection? = [ pixel0, pixel1, pixel2, ...]
 """
-
 #! OD TUKAJ NAPREJ POČNEŠ KAR HOČEŠ !!!
+"""
+def create_list(dicta):
+    li = []
+    for i in library:
+        for color in library[i]:
+            print(color)
+            for det in library[i][color]:
+                #pridobimo 50 samplov
+                #print(det)
+                for it in range(100):
+                    tem = random.sample(det,100)
+                    #flattenamo sample
+                    flatten_tem = functools.reduce(operator.iconcat,tem,[])
+                    #appendamo color
+                    flatten_tem.append(color)
+                    #dodamo v li
+                    li.append(flatten_tem)
+    return li
+"""
+
+#datF = pd.DataFrame(create_list(library))
+#print(datF.head)
+#datF.to_pickle("./pixls100.pkl")
+
+datF = pd.read_pickle("pixls.pkl")
+#print(datF.count())
+
+
+df_x = datF.iloc[:,0:-1].to_numpy()
+df_y = datF.iloc[:,-1].to_numpy()
+#df_x =
+#print(df_x)
+#print(df_y)
+
+randomForest = RandomForestClassifier(n_estimators=100
+                                    , criterion="gini"
+                                    , max_depth=None
+                                    , min_samples_split=2
+                                    , min_samples_leaf=1
+                                    , min_weight_fraction_leaf=0.0
+                                    , max_features="auto"
+                                    , max_leaf_nodes=None
+                                    , min_impurity_decrease=0.0
+                                    , bootstrap=True
+                                    , oob_score=True
+                                    , n_jobs=-1
+                                    , random_state=None
+                                    , verbose=0
+                                    , warm_start=False
+                                    , class_weight=None
+                                    , ccp_alpha=0.0
+                                    , max_samples=400
+                                    )
+
+randomForest_fit = randomForest.fit(df_x,df_y)
+print("\nRandom Forest:")
+print(f"\tscore: {randomForest_fit.oob_score_}\n")
+filename = "random_forestV2"
+pickle.dump(randomForest_fit, open(f"{filename}.sav", 'wb'))
+"""
 # dictionary je v spremenljivki "library"
 allColors = ["r","g","b","y","c"]
 numOfAllDetections = 0
@@ -91,7 +163,6 @@ for color in allColors:
     numOfAllDetections += len(temp2)
     numOfAllPixels += sum([len(x) for x in temp1])
     numOfAllPixels += sum([len(x) for x in temp2])
-
 print()
 print(f"--------------------- num of detections ---------------------")
 for objectType in ["ring", "cylinder"]:
@@ -128,6 +199,4 @@ for color in allColors:
     temp2 = [len(x) for x in library["cylinder"][color]]
     print(f"{color}: {sum(temp1)+sum(temp2)}\t( {round((sum(temp1)+sum(temp2))/numOfAllPixels*100)} % )")
 print()
-
-
-
+"""
